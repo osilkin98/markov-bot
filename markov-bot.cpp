11b9e29612e747b4c1b43372bb2bd50d44d81662
Oleg Silkin <o.silkin98@gmail.com>
2018-04-07 20:35:51 -0500

    markov-bot.cpp added

---
+++ markov-bot.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+class markov_chain {
+public:
+  std::string generate(const std::string& input) const {
+    
+  }
+  
+}
